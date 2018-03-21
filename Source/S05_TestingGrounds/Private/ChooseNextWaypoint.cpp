94ab84d458fb556ff55d05cd56a29e92448493a8
Growiel <girardol182@gmail.com>
2018-03-22 00:50:33 +0100

    Add the BTTask in C++, with only a log to see if it works for now.

---
+++ Source/S05_TestingGrounds/Private/ChooseNextWaypoint.cpp
@@ -0,0 +1,9 @@
+// Growiel
+
+#include "ChooseNextWaypoint.h"
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	UE_LOG(LogTemp, Warning, TEXT("BOB: Execute task c++"));
+	return EBTNodeResult::Succeeded;
+}
