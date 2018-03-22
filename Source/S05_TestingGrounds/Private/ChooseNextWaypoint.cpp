69b4fc4a67729f0d8ae280ebe1262c0e2854280d
Growiel <girardol182@gmail.com>
2018-03-22 01:15:35 +0100

    S5L223: Access the blackboard values and use UFUNCTION to expose a variable.

--- Source/S05_TestingGrounds/Private/ChooseNextWaypoint.cpp
+++ Source/S05_TestingGrounds/Private/ChooseNextWaypoint.cpp
@@ -1,9 +1,14 @@
 // Growiel
 
 #include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	UE_LOG(LogTemp, Warning, TEXT("BOB: Execute task c++"));
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+
+	UE_LOG(LogTemp, Warning, TEXT("Waypoint index: %i"), Index);
+
 	return EBTNodeResult::Succeeded;
 }
